8a1bd773818d678dfebe5436a242489c08d7e203
npeyre <90184564+npeyre@users.noreply.github.com>
2023-01-15 15:48:10 +0000

    Added notes about chatterbot_corpus

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,11 @@
 # Chatbot is a software programmed to interact with humans, using Natural Language Processing (NLP)
+
+# Install Chatterbot Python Library
 # Documentation: https://chatterbot.readthedocs.io/en/stable/tutorial.html
 # pip install chatterbot 
 # if the above command doesnt work, try installing the actual version or a version below (aacording to stackoverflow)
 # pip install chatterbot==1.0.4
+
+# Install a machine readable multilingual dialog corpus modules
+# These modules are used to quickly train ChatterBot to respond to various inputs in different languages
+# pip install chatterbot_corpus

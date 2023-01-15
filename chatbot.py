fef40d4a8dcf62210eeee0270f2350bd9826b771
npeyre <90184564+npeyre@users.noreply.github.com>
2023-01-15 16:23:30 +0000

    Added bot name, small talk and list trainer

--- chatbot.py
+++ chatbot.py
@@ -9,3 +9,50 @@
 # Install a machine readable multilingual dialog corpus modules
 # These modules are used to quickly train ChatterBot to respond to various inputs in different languages
 # pip install chatterbot_corpus
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# Define the bot, create a new instance of a chatbot class
+# Pass parameters: name, read_only, logic adapters
+# read_only=True disables bots ability to learn; read_only=False enables ability to learn
+# logic adapters is a list of adapters used to train the bot
+# multiple adapters can be entered
+# The BestMatch logic adapter selects a response based on the best known match to a given statement.
+# The MathematicalEvaluation logic adapter checks a given statement to see if it contains a mathematical expression that can be evaluated.
+# The TimeLogicAdapter identifies statements in which a question about the current time is asked. 
+
+
+bot = ChatBot(name='Cleo', read_only=True, logic_adapters=["chatterbot.logic.MathematicalEvaluation", "chatterbot.logic.BestMatch","chatterbot.logic.TimeLogicAdapter" ])
+#bot = ChatBot('Cleo')
+
+# Define the list of responses
+small_talk = ['hi there!',
+          'hi!',
+          'how do you do?',
+          'how are you?',
+          'i\'m cool.',
+          'fine, you?',
+          'always cool.',
+          'i\'m ok',
+          'glad to hear that.',
+          'i\'m fine',
+          'glad to hear that.',
+          'i feel awesome',
+          'excellent, glad to hear that.',
+          'not so good',
+          'sorry to hear that.',
+          'what\'s your name?',
+          'i\'m Cleo. Ask me a math question, please.']
+math_talk_1 = ['pythagorean theorem',
+          'a squared plus b squared equals c squared.']
+math_talk_2 = ['law of cosines',
+          'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
+
+
+# create and train the bot by creating an instance of ListTrainer and supplying it with the lists of strings
+list_trainer = ListTrainer(bot)
+for item in (small_talk, math_talk_1, math_talk_2):
+    list_trainer.train(item)
+
+# The bot should now be trained and ready to communicate

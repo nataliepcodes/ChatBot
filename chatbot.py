df061c03b8190ce1ae5d9f36449ee1ed19ae2dab
npeyre <90184564+npeyre@users.noreply.github.com>
2023-01-15 15:40:49 +0000

    Updated chatbot.py file with comments on library installation

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,5 @@
-# Chatbot is a software programmed to interact with humans, using Artificial Intelligence / Natural Language Processing
-# pip install chatterbot
+# Chatbot is a software programmed to interact with humans, using Natural Language Processing (NLP)
+# Documentation: https://chatterbot.readthedocs.io/en/stable/tutorial.html
+# pip install chatterbot 
+# if the above command doesnt work, try installing the actual version or a version below (aacording to stackoverflow)
+# pip install chatterbot==1.0.4
